1b4d19be5acc50801a5e6c7f7670ea1a75b6ebad
Alberto <albertomiguelgarciasimo@outlook.com>
2024-05-02 18:00:41 +0200

    Rama Alberto

---
+++ src/alberto/alberto.java
@@ -0,0 +1,18 @@
+package alberto;
+
+public class alberto {
+
+	public static void main(String[] args) {
+		
+		miNombre();
+
+	}
+	
+
+	public static void miNombre(){
+		System.out.println("Me llamo Alberto");
+	}
+	
+	
+
+}
