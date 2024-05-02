030b584ef29322ead11ad69814f0b45f053dcda1
mJangelm <39463320+mJangelm@users.noreply.github.com>
2024-05-02 18:41:19 +0200

    Rama Juan

---
+++ src/juan/juan.java
@@ -0,0 +1,12 @@
+package juan;
+
+public class juan {
+
+	public static void main(String[] args) {
+		minombre();
+
+	}
+private static void minombre() {
+	System.out.println("hola me llamo Juan Ángel");
+}
+}
